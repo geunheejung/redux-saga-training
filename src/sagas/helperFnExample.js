eccd1698181090a30eeae13234ac0db99d14603f
정근희 <jeong-geunhui@jeong-geunhuiui-MacBook-Pro.local>
2018-09-14 01:44:26 +0900

    중간정리

--- src/sagas/helperFnExample.js
+++ src/sagas/helperFnExample.js
@@ -1,5 +1,5 @@
 import {delay} from 'redux-saga'
-import {fork, call, put, take, takeEvery} from 'redux-saga/effects';
+import {fork, call, put, take, takeEvery, cancel} from 'redux-saga/effects';
 import * as fromReduxs from '../reducers';
 
 // function* takeEvery(pattern, saga, ...args) {
@@ -11,6 +11,28 @@ import * as fromReduxs from '../reducers';
 //   });
 //   return task
 // }
+
+function* mockTakeLatest(pattern, saga, ...args) {
+  const task = yield fork(function* () {
+    let lastTask;
+    let watchingActionCount = 0;
+
+    while (true) {
+      const action = yield take(pattern);
+      watchingActionCount++;
+      // 가장 마지막 action만 take하기 위해 이전 action에 대한 task는 제거하고 현재 fork한 task만 남김.
+      if (lastTask) {
+        yield cancel(lastTask);
+        yield console.log(`lastTask ---> (${lastTask}) ////// actionWacthingCount ---> (${watchingActionCount})`);
+      }
+
+
+      lastTask = yield fork(saga, ...args.concat(action));
+    }
+  });
+
+  return task;
+}
 
 function* mockTakeEvery(pattern, saga, ...args) {
   while (true) {
@@ -26,8 +48,9 @@ function* subTask() {
 
 function* mainFlow() {
   console.log('hello');
-  yield mockTakeEvery('MOCK_FETCH_ACTION', subTask);
+  // yield mockTakeEvery('MOCK_FETCH_ACTION', subTask);
   // 이처럼 takeEvery에서 while(true) {} task를 fork이펙트로 non-blocking 해주지 않으면 mainFlow에서는 takeEvery가 blocking하기때문에 다음 yield로 진행이 안된다.
+  yield mockTakeLatest('MOCK_FETCH_ACTION', subTask);
   console.log('END MAIN FLOW ---> (1)');
 }
 
@@ -37,4 +60,7 @@ function* mainFlow2() {
   yield console.log('END MAIN FLOW ---> (2)');
 }
 
-export default [mainFlow, mainFlow2]
+export default [
+  mainFlow,
+  // mainFlow2
+]
